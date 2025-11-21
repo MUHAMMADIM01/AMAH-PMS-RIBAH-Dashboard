c5fe0e7b83bab977f3d1848963104fa3500edfdf
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-11-21 14:09:21 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,39 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqXjy7ogO8qMCsfuYB61yBpZJpKKKGhqk",
-  authDomain: "amahmedicinestore-be61b.firebaseapp.com",
-  databaseURL: "https://amahmedicinestore-be61b-default-rtdb.firebaseio.com",
-  projectId: "amahmedicinestore-be61b",
-  storageBucket: "amahmedicinestore-be61b.firebasestorage.app",
-  messagingSenderId: "740301709370",
-  appId: "1:740301709370:web:9fd66894daa25bd85b1503",
-  measurementId: "G-NV6KNSM3CV"
+  apiKey: "AIzaSyC6Y6IHVFWQ3MoufpQpPod-a2tiE2mm3fU",
+  authDomain: "amah-store.firebaseapp.com",
+  databaseURL: "https://amah-store-default-rtdb.firebaseio.com",
+  projectId: "amah-store",
+  storageBucket: "amah-store.firebasestorage.app",
+  messagingSenderId: "673483173814",
+  appId: "1:673483173814:web:e4fc948ed749bb31c09b17"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+// Firestore
+export const db = getFirestore(app);
+
+// Storage
+export const storage = getStorage(app);
+
+// Firestore helpers
+export const MedicinesCollection = collection(db, "medicines");
+export { addDoc, getDocs, deleteDoc, doc, ref, uploadBytes, getDownloadURL };
