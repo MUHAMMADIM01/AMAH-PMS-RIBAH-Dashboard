6ed8f0a01770dc58830ce73c52927b98acd123b9
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-11-24 12:14:25 +0100

    Update login.js

--- admin/login.js
+++ admin/login.js
@@ -1,4 +1,4 @@
-// admin-login.js
+// login.js
 
 document.getElementById("loginBtn").addEventListener("click", function (e) {
   e.preventDefault();
@@ -6,13 +6,13 @@ document.getElementById("loginBtn").addEventListener("click", function (e) {
   const username = document.getElementById("adminUser").value.trim();
   const password = document.getElementById("adminPass").value.trim();
 
-  // DEFAULT ADMIN LOGIN
-  const correctUser = "admin";
-  const correctPass = "12345";
+  // DEFAULT ADMIN CREDENTIALS
+  const ADMIN_USER = "admin";
+  const ADMIN_PASS = "12345";
 
-  if (username === correctUser && password === correctPass) {
+  if (username === ADMIN_USER && password === ADMIN_PASS) {
     alert("Login Successful!");
-    window.location.href = "dashboard.html"; // Admin page
+    window.location.href = "dashboard.html";
   } else {
     alert("Incorrect Username or Password!");
   }
