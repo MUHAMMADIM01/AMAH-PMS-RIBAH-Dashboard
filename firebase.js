81fe3bce898f1c6de6fe68c974e44cf161c2b005
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-11-26 16:31:50 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,7 @@
+// firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-const firebaseConfig = { apiKey:'', authDomain:'', projectId:'' };
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 
 const firebaseConfig = {
   apiKey: "AIzaSyC6Y6IHVFWQ3MoufpQpPod-a2tiE2mm3fU",
@@ -13,3 +12,10 @@ const firebaseConfig = {
   messagingSenderId: "673483173814",
   appId: "1:673483173814:web:e4fc948ed749bb31c09b17"
 };
+
+// Kada a maimaita firebaseConfig sau biyu !!!
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+console.log("Firebase connected!");
