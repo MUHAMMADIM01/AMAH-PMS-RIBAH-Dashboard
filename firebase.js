3e26bd2bf7ca452c0e0347e71da0b57feaaf98a1
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-11-28 17:42:12 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC6Y6IHVFWQ3MoufpQpPod-a2tiE2mm3fU",
-    authDomain: "amah-store.firebaseapp.com",
-    projectId: "amah-store",
-    storageBucket: "amah-store.firebasestorage.app",
-    messagingSenderId: "673483173814",
-    appId: "1:673483173814:web:e4fc948ed749bb31c09b17"
+  apiKey: "AIzaSyC6Y6IHVFWQ3MoufpQpPod-a2tiE2mm3fU",
+  authDomain: "amah-store.firebaseapp.com",
+  databaseURL: "https://amah-store-default-rtdb.firebaseio.com",
+  projectId: "amah-store",
+  storageBucket: "amah-store.firebasestorage.app",
+  messagingSenderId: "673483173814",
+  appId: "1:673483173814:web:e4fc948ed749bb31c09b17"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
