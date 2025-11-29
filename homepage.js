ba46f34be73323bc89cc6958be2bdef3b5ad96b1
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-11-29 11:15:57 +0100

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,3 +1,4 @@
+
 import { db } from "./firebase.js";
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
@@ -21,29 +22,3 @@ async function loadTips() {
 
 loadTips();
 
-import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-async function loadMedicines() {
-    const container = document.getElementById("medList");
-    container.innerHTML = "";
-
-    const querySnapshot = await getDocs(collection(db, "medicines"));
-
-    querySnapshot.forEach((doc) => {
-        const med = doc.data();
-
-        const item = document.createElement("div");
-        item.classList.add("medicine-item");
-
-        item.innerHTML = `
-            <h3>${med.name}</h3>
-            <p>${med.desc}</p>
-            ${med.imageURL ? `<img src="${med.imageURL}" class="med-image">` : ""}
-        `;
-
-        container.appendChild(item);
-    });
-}
-
-loadMedicines();
