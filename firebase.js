0f4aa382790e22148e7b4f9afd901fdccb019b46
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-12-02 00:13:29 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,21 @@
-// Import Firebase CDN modules
+// firebase.js
+// --- Replace the firebaseConfig object with YOUR project's keys ---
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-// Correct Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyC6Y6IHVFWQ3MoufpQpPod-a2tiE2mm3fU",
   authDomain: "amah-store.firebaseapp.com",
   databaseURL: "https://amah-store-default-rtdb.firebaseio.com",
   projectId: "amah-store",
-  storageBucket: "amah-store.appspot.com",   // ← FIXED!!!
+  storageBucket: "amah-store.firebasestorage.app",
   messagingSenderId: "673483173814",
   appId: "1:673483173814:web:e4fc948ed749bb31c09b17"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export services
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
