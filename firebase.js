bd125e1baf401717691caf267efb4d7d2c13617b
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-12-05 14:28:22 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,28 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
+// Firebase App (core SDK)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 
+// Firestore
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+// Auth
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+// Storage
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
+
+// Your Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyC6Y6lHFWQ3MoufpQPpPod-a...",
+  apiKey: "AIzaSyC6Y6IHVFWQ3MoufpQpPod-a2tiE2mm3fU",
   authDomain: "amah-store.firebaseapp.com",
+  databaseURL: "https://amah-store-default-rtdb.firebaseio.com",
   projectId: "amah-store",
   storageBucket: "amah-store.firebasestorage.app",
   messagingSenderId: "673483173814",
   appId: "1:673483173814:web:e4fc948ed749bb31c09b17"
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
